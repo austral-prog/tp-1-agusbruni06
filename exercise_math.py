5207818050edc222ae784445e083c18942b4bd88
agusbruni06 <agus.bruni2019@gmail.com>
2025-08-11 12:35:36 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,13 @@
+#EJERCICIO 2
 def math():
     a = 57
     b = 7
+    suma = a + b
+    diferencia = a - b
+    producto = a * b
+    promedio = (a + b) / 2
+    cociente = int(a / b)
+    resto = int(a % b)
+    cocienteReal = a / b
+
+
